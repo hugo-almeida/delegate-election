869052af4340ba09553214ec486ff1a7bbdab410
frozenprawn <ricardo_fmpires@hotmail.com>
2015-05-20 18:56:27 +0100

    Implemented voting Added student attributes

--- src/main/java/core/ElectionPeriod.java
+++ src/main/java/core/ElectionPeriod.java
@@ -1,19 +1,28 @@
 package core;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ElectionPeriod extends Period {
 
-    private final Map<Integer, Vote> votes;
+    private final Map<Integer, List<Vote>> votes;
 
     public ElectionPeriod(Date start, Date end) {
         super(start, end);
-        votes = new HashMap<Integer, Vote>();
+        votes = new HashMap<Integer, List<Vote>>();
     }
 
     public void vote(int voter, int voted) {
-        votes.put(voter, new Vote(voter, voted));
+        final Vote v = new Vote(voter, voted);
+        if (votes.containsKey(voted)) {
+            votes.get(voted).add(v);
+        } else {
+            votes.put(voted, new ArrayList<Vote>());
+            votes.get(voted).add(v);
+        }
+
     }
 }
--- src/main/java/core/Student.java
+++ src/main/java/core/Student.java
@@ -1,5 +1,29 @@
 package core;
 
 public class Student {
+    private final String name;
+    private final int number;
+    private final String email;
 
+    //private whatever photo;
+
+    public Student(String name, int number, String email /*whatever photo*/) {
+        this.name = name;
+        this.number = number;
+        this.email = email;
+        //this.photo = photo;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
