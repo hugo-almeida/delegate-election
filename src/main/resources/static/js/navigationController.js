45fb1dbb30cb4c0621092dabe45b38cdb6a53e26
frozenprawn <ricardo_fmpires@hotmail.com>
2015-06-29 15:27:38 +0100

    Fixed bugs Cleaned up search function Added debug features for vote/application periods Added support for multiple degrees per person to the frontend (still unsupported on the backend)

--- src/main/resources/static/js/navigationController.js
+++ src/main/resources/static/js/navigationController.js
@@ -1,10 +1,21 @@
-angular.module('delegados').controller('navigationCtrl', ['$rootScope', '$scope', '$http',
-                                                          function($rootScope, $scope, $http)  {
+angular.module('delegados').controller('navigationCtrl', ['$rootScope', '$scope', '$http', '$log',
+                                                          function($rootScope, $scope, $http, $log)  {
 	$http.get('user').success(function(data) {
 		if (data) {
 			$rootScope.authenticated = true;
 			$rootScope.id = data.name;
 			$rootScope.credentials = data;
+			$rootScope.degrees = [];
+			for (index in data.roles) {
+				//$log.log(data.roles[index]);
+				if(data.roles[index].type=='STUDENT') {
+					$rootScope.degrees.push(data.roles[index]);
+				//	$log.log('pushed');
+				}
+			}
+			$rootScope.appPeriod = false;
+			$rootScope.votePeriod = false;
+			//$log.log($rootScope.degrees[0].registrations[0].name)
 		} else {
 			$rootScope.authenticated = false;
 		}
@@ -24,4 +35,15 @@ angular.module('delegados').controller('navigationCtrl', ['$rootScope', '$scope'
 			$rootScope.authenticated = false;
 		});
 	};
+	
+	$scope.debugApplyPeriod = function() {
+		$rootScope.appPeriod = !$rootScope.appPeriod;
+	}
+	
+	$scope.debugVotePeriod = function() {
+		$rootScope.votePeriod = !$rootScope.votePeriod;
+	}
+	
+	$rootScope.debug = true;
 }]);
+
