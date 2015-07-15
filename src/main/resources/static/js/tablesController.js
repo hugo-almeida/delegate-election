19b9b902693f08b8fd1bc1323363f0b6e5bcae3a
frozenprawn <ricardo_fmpires@hotmail.com>
2015-07-15 16:45:34 +0100

    Added functionality to admin page

--- src/main/resources/static/js/tablesController.js
+++ src/main/resources/static/js/tablesController.js
@@ -1,15 +1,59 @@
 angular.module('delegados').controller('tablesCtrl', ['$rootScope', '$scope', '$http', '$log',
                                                           function(rc, sc, http, log)  {
-	sc.active = 'Candidaturas';
 	
-	sc.activeTab = function(tab) {
-		if(sc.active == tab)
-			return true;
-		else return false;
-	}
+	sc.loaded = false;
 	
-	sc.setActiveTab = function (tab) {
-		sc.active = tab;
-		log.log(sc.active);
+	sc.allSelected = false;
+	
+	sc.loadDegrees = function() {
+		http.get('periods').success(function(data){
+			sc.degrees = data;
+			sc.loaded = true;
+		});
+	};
+	
+	sc.loadDegrees();
+	
+	sc.selection = [];
+	
+	sc.toggleSelection = function(degree) {
+		var index = sc.selection.indexOf(degree);
+		
+		if(index > -1) {
+			sc.selection.splice(index, 1);
+		}
+		else {
+			sc.selection.push(degree);
+		}
+	};
+	
+	sc.toggleSelectAll = function() {
+		log.log('hi');
+		if(!sc.allSelected) {
+			log.log('toggling off')
+			for(degree in sc.degrees) {
+				log.log(sc.degrees[degree])
+				var name = sc.degrees[degree].degree;
+				var index = sc.selection.indexOf(name);
+				
+				if(index <= -1) {
+					sc.selection.push(name);
+				}
+			}
+			sc.allSelected = true;
+		}
+		else {
+			log.log('toggling on')
+			for(degree in sc.degrees) {
+				log.log(sc.degrees[degree])
+				var name = sc.degrees[degree].degree;
+				var index = sc.selection.indexOf(name);
+				
+				if(index > -1) {
+					sc.selection.splice(index, 1);
+				}
+			}
+			sc.allSelected = false;
+		}
 	}
 }]);
