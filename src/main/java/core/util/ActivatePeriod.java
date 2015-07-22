5e20a5645a19b14ee50c16c8e57f91c7da582dde
hugo-almeida <hugoalmeida1211@gmail.com>
2015-07-22 17:03:43 +0100

    Test Periods now created on Degree Year 2

--- src/main/java/core/util/ActivatePeriod.java
+++ src/main/java/core/util/ActivatePeriod.java
@@ -4,21 +4,24 @@ import java.util.TimerTask;
 
 import core.DegreeYear;
 import core.Period;
+import core.PeriodDAO;
 
 public class ActivatePeriod extends TimerTask {
 
-    private final DegreeYear courseYear;
+    private final DegreeYear degreeYear;
     private final Period period;
+    private final PeriodDAO periodDAO;
 
-    public ActivatePeriod(DegreeYear courseYear, Period period) {
+    public ActivatePeriod(Period period, PeriodDAO dao) {
         super();
-        this.courseYear = courseYear;
+        this.degreeYear = period.getDegreeYear();
         this.period = period;
+        periodDAO = dao;
     }
 
     @Override
     public void run() {
-        courseYear.setActivePeriod(period);
+        degreeYear.setActivePeriod(period);
+        periodDAO.save(period);
     }
-
 }
