b70bd044524d325f218667ce62be2eb334f2319f
frozenprawn <ricardo_fmpires@hotmail.com>
2015-09-14 18:35:50 +0100

    Added angular-ui support

--- src/main/resources/static/js/controllers/bulkEditController.js
+++ src/main/resources/static/js/controllers/bulkEditController.js
@@ -1,6 +1,38 @@
 angular.module('delegados').controller('bulkCtrl', ['$rootScope', '$scope', '$http', '$log', 'bulkEdit', 'formatDate',
                                                           function(rc, sc, http, log, bulkEdit, formatDate)  {
 	
+    sc.today = function() {
+	    sc.dt = new Date();
+	};
+	sc.today();
+	
+    sc.clear = function () {
+    	sc.dt = null;
+	};
+	
+	sc.openAppStart = function($event) {
+	  sc.status.AppStartOpened = true;
+	};
+	
+	sc.openAppEnd = function($event) {
+		sc.status.AppEndOpened = true;
+	};
+	
+	sc.openElecStart = function($event) {
+		sc.status.ElecStartOpened = true;
+	};
+			
+	sc.openElecEnd = function($event) {
+		sc.status.ElecEndOpened = true;
+	};
+		
+	sc.status = {
+	  AppStartOpened: false,
+	  AppEndOpened: false,
+	  ElecStartOpened: false,
+	  ElecEndOpened: false
+	};	
+	
 	sc.applicationStart = new Date();
 	sc.applicationEnd = new Date();
 	sc.electionStart = new Date();
