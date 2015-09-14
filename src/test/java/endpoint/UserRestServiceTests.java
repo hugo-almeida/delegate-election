e044461493e4970a4e532072842285d856dd2777
Fernando Santos <fernandojosepsantos@gmail.com>
2015-09-14 16:22:33 +0100

    Testes ja sao executados com sucesso, em principio. Falta so implementar o resto dos testes.

--- src/test/java/endpoint/UserRestServiceTests.java
+++ src/test/java/endpoint/UserRestServiceTests.java
@@ -1,13 +1,17 @@
 package endpoint;
 
+import static com.jayway.restassured.module.mockmvc.RestAssuredMockMvc.when;
+import static org.hamcrest.Matchers.emptyIterable;
+import static org.hamcrest.Matchers.equalTo;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.context.WebApplicationContext;
 
 import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc;
 
@@ -17,43 +21,71 @@ import core.Degree;
 import core.Degree.DegreeType;
 import core.DegreeYear;
 import core.Student;
+import core.StudentDAO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = App.class)
 @WebIntegrationTest
-//@IntegrationTest("server.port:0")
 public class UserRestServiceTests {
 
-    @Autowired // 5
-    CalendarDAO repository;
+    @Autowired
+    CalendarDAO calendarRepository;
 
-    @Value("${local.server.port}") // 6
-    int port;
+    @Autowired
+    StudentDAO studentRepository;
+
+    @Autowired
+    WebApplicationContext webApplicationContext;
+
+    Student studentOne;
+    Student studentTwo;
+    Degree degreeOne;
+    Degree degreeTwo;
+    DegreeYear firstDegreeYear;
 
     @Before
     public void setUp() {
-        repository.deleteAll();
+        calendarRepository.deleteAll();
 
-        Calendar calendar = new Calendar(3);
-        Degree degree = new Degree("", "", "1", DegreeType.Bachelor.toString(), calendar);
-        calendar.addDegree(degree);
-        degree.initDegreeYears();
-        DegreeYear firstDegreeYear = degree.getDegreeYear(1);
-        Student me = new Student("Fernando", "ist173833", "stuff@gmail.com", "", "");
-        me.setDegreeYear(firstDegreeYear);
-        firstDegreeYear.addStudent(me);
+        Calendar calendar = new Calendar(1);
+        degreeOne = new Degree("Degree1", "deg1", "1", DegreeType.Bachelor.toString(), calendar);
+        calendar.addDegree(degreeOne);
+        degreeTwo = new Degree("Degree2", "deg2", "2", DegreeType.Master.toString(), calendar);
+        calendar.addDegree(degreeTwo);
+        degreeOne.initDegreeYears();
+        degreeTwo.initDegreeYears();
 
-        repository.save(calendar);
+        firstDegreeYear = degreeOne.getDegreeYear(1);
 
-        // 9
-//        RestAssuredMockMvc.basePath = "http://localhost:8080";
-        RestAssuredMockMvc.standaloneSetup(new Controller());
+        studentOne = new Student("Student1", "id1", "email1@email.com", "", "");
+        studentTwo = new Student("Student2", "id2", "email2@email.com", "", "");
+
+        calendarRepository.save(calendar);
+
+        RestAssuredMockMvc.webAppContextSetup(webApplicationContext);
     }
 
-    // 10
     @Test
-    public void canFetchStudentDegrees() {
-        RestAssuredMockMvc.when().get("/students/{istId}/degrees", "ist173833").then().statusCode(200);
+    public void studentWithNoDegressTest() {
+        studentTwo.setDegreeYear(firstDegreeYear);
+        firstDegreeYear.addStudent(studentTwo);
+
+        calendarRepository.save(calendarRepository.findFirstByOrderByYearDesc());
+
+        when().get("/students/{istId}/degrees", studentOne.getUsername()).then().assertThat().statusCode(200).body("$",
+                emptyIterable());
+    }
+
+    // Not working
+    @Test
+    public void studentWithOneDegressTest() {
+        studentOne.setDegreeYear(firstDegreeYear);
+        firstDegreeYear.addStudent(studentOne);
+
+        calendarRepository.save(calendarRepository.findFirstByOrderByYearDesc());
+
+        when().get("/students/{istId}/degrees", studentOne.getUsername()).then().assertThat().statusCode(200).body("$[0].id",
+                equalTo(degreeOne.getId()));
     }
 
 }
